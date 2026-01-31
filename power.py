a508ba4db3d9808ad6b4a732fce8dfffa7cf350a
pavan <pavanhn057@gmail.com>
2026-01-31 21:30:19 +0530

    pavan

---
+++ power.py
@@ -0,0 +1,6 @@
+<form action="/submit" method="post">
+    Name: <input type="text" name="name"><br><br>
+    Email: <input type="email" name="email"><br><br>
+    Password: <input type="password" name="pwd"><br><br>
+    <input type="submit" value="Submit">
+</form>
