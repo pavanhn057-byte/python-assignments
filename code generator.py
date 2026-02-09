29cd4ade531dd24258fa2508f790779f276d9909
pavn <pavnhn057@gmail.com>
2026-02-09 10:56:27 +0530

    pavan

---
+++ code generator.py
@@ -0,0 +1,17 @@
+# Basic Generator Function
+
+def simple_generator():
+    print("Start")
+    yield 1
+    print("Middle")
+    yield 2
+    print("End")
+    yield 3
+
+
+# Using the generator
+gen = simple_generator()
+
+print(next(gen))  # First value
+print(next(gen))  # Second value
+print(next(gen))  # Third value
